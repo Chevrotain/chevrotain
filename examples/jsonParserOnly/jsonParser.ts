4278c81d2b82abddc287c8057426aad5df7847d2
Shachar Soel <shachar.soel@sap.com>
2015-02-22 17:15:37 +0200

    finished first version of the json parser example.
    Change-Id: I91fe6994b711699b9952d73ee9c2dd1ee3b0c3d8

--- examples/jsonParserOnly/jsonParser.ts
+++ examples/jsonParserOnly/jsonParser.ts
@@ -6,14 +6,13 @@ module chevrotain.examples.json {
     import recog = chevrotain.parse.infra.recognizer;
     import tok = chevrotain.scan.tokens;
 
-
     export class StringTok extends tok.Token {}
     export class NumberTok extends tok.Token {}
     export class TrueTok extends tok.Token {}
     export class FalseTok extends tok.Token {}
     export class NullTok extends tok.Token {}
-    export class LParenTok extends tok.Token {}
-    export class RParenTok extends tok.Token {}
+    export class LCurlyTok extends tok.Token {}
+    export class RCurlyTok extends tok.Token {}
     export class LSquareTok extends tok.Token {}
     export class RSquareTok extends tok.Token {}
     export class CommaTok extends tok.Token {}
@@ -22,9 +21,8 @@ module chevrotain.examples.json {
 
     export class JsonParser extends recog.BaseRecognizer {
 
-
         object():void {
-            this.CONSUME(LParenTok);
+            this.CONSUME(LCurlyTok);
             this.OPTION(isString, ()=> {
                 this.objectItem();
                 this.MANY(isAdditionalItem, ()=> {
@@ -32,7 +30,7 @@ module chevrotain.examples.json {
                     this.objectItem();
                 });
             });
-            this.CONSUME(RParenTok);
+            this.CONSUME(RCurlyTok);
         }
 
         objectItem():void {
@@ -54,25 +52,27 @@ module chevrotain.examples.json {
 
         value():void {
             this.OR(
-                [{
-                    WHEN: null, THEN_DO: ()=> {
-                    }
-                },
-                    {
-                        WHEN: null, THEN_DO: ()=> {
+                [
+                    // @formatter:off
+                    {WHEN: isString, THEN_DO: ()=> {
+                        this.CONSUME(StringTok)}},
+                    {WHEN: isNumber, THEN_DO: ()=> {
+                        this.CONSUME(NumberTok)}},
+                    {WHEN: isObject, THEN_DO: ()=> {
+                        this.object()}},
+                    {WHEN: isArray, THEN_DO: ()=> {
+                        this.array()}},
+                    {WHEN: isTrue, THEN_DO: ()=> {
+                        this.CONSUME(TrueTok)}},
+                    {WHEN: isFalse, THEN_DO: ()=> {
+                        this.CONSUME(FalseTok)}},
+                    {WHEN: isNull, THEN_DO: ()=> {
+                        this.CONSUME(NullTok)}}
+                    // @formatter:off
 
-                    }
-                    },
-                    {
-                        WHEN: null, THEN_DO: ()=> {
-
-                    }
-                    }
-                ], 'bamba')
+                ], 'add error message')
         }
-
     }
-
 
     function isString():boolean {
         return this.NEXT_TOKEN() instanceof StringTok;
@@ -82,10 +82,28 @@ module chevrotain.examples.json {
         return this.NEXT_TOKEN() instanceof CommaTok;
     }
 
-    function isValue():boolean {
-        // TODO: impel
-        return false
+    function isNumber():boolean {
+        return this.NEXT_TOKEN() instanceof NumberTok;
     }
 
+    function isObject():boolean {
+        return this.NEXT_TOKEN() instanceof LCurlyTok;
+    }
+
+    function isArray():boolean {
+        return this.NEXT_TOKEN() instanceof LSquareTok;
+    }
+
+    function isTrue():boolean {
+        return this.NEXT_TOKEN() instanceof TrueTok;
+    }
+
+    function isFalse():boolean {
+        return this.NEXT_TOKEN() instanceof FalseTok;
+    }
+
+    function isNull():boolean {
+        return this.NEXT_TOKEN() instanceof NullTok;
+    }
 
 }
