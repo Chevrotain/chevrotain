005c134a03bd1a0e04c1b11275e3de2590a12aae
Shahar Soel <shahar.soel@gmail.com>
2015-05-08 19:13:16 +0300

    make sure composed keys used in the cache can never be valid rule names. '~' is not valid in a javascript identifer thus not valid in a rule name.

--- test/parse/grammar/follow_spec.ts
+++ test/parse/grammar/follow_spec.ts
@@ -20,12 +20,12 @@ module chevrotain.follow.spec {
         it("can build a followNamePrefix from a Terminal", function () {
             var terminal = new gast.Terminal(t.IdentTok)
             var actual = buildInProdFollowPrefix(terminal)
-            expect(actual).toBe("Ident1_IN_")
+            expect(actual).toBe("Ident1_~IN~_")
 
             var terminal2 = new gast.Terminal(t.EntityTok)
             terminal2.occurrenceInParent = 3
             var actual2 = buildInProdFollowPrefix(terminal2)
-            expect(actual2).toBe("Entity3_IN_")
+            expect(actual2).toBe("Entity3_~IN~_")
         })
 
         it("can build a followName prefix from a TopLevel Production and index", function () {
@@ -33,19 +33,19 @@ module chevrotain.follow.spec {
             var index = 5
 
             var actual = buildBetweenProdsFollowPrefix(prod, index)
-            expect(actual).toBe("bamba5_IN_")
+            expect(actual).toBe("bamba5_~IN~_")
         })
 
         it("can compute the follows for Top level production ref in ActionDec", function () {
             var actual:any = new ResyncFollowsWalker(samples.actionDec).startWalking()
             var actualFollowNames = actual.keys()
             expect(actualFollowNames.length).toBe(3)
-            expect(actual.get("paramSpec1_IN_actionDec").length).toBe(2)
-            matchers.arrayEqualityNoOrder(actual.get("paramSpec1_IN_actionDec"), [t.CommaTok, t.RParenTok])
-            expect(actual.get("paramSpec2_IN_actionDec").length).toBe(2)
-            matchers.arrayEqualityNoOrder(actual.get("paramSpec1_IN_actionDec"), [t.CommaTok, t.RParenTok])
-            expect(actual.get("qualifiedName1_IN_actionDec").length).toBe(1)
-            matchers.arrayEqualityNoOrder(actual.get("qualifiedName1_IN_actionDec"), [t.SemicolonTok])
+            expect(actual.get("paramSpec1_~IN~_actionDec").length).toBe(2)
+            matchers.arrayEqualityNoOrder(actual.get("paramSpec1_~IN~_actionDec"), [t.CommaTok, t.RParenTok])
+            expect(actual.get("paramSpec2_~IN~_actionDec").length).toBe(2)
+            matchers.arrayEqualityNoOrder(actual.get("paramSpec1_~IN~_actionDec"), [t.CommaTok, t.RParenTok])
+            expect(actual.get("qualifiedName1_~IN~_actionDec").length).toBe(1)
+            matchers.arrayEqualityNoOrder(actual.get("qualifiedName1_~IN~_actionDec"), [t.SemicolonTok])
         })
 
         it("can compute all follows for a set of top level productions", function () {
