a9b20a134d674bb969898a600064317c8ba3e537
Shahar Soel <shahar.soel@gmail.com>
2015-07-25 00:44:06 +0300

    upgrade to tsc 1.5.3

--- watch.bat
+++ watch.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Git\bin\sh.exe" --login -i -c "find ./src/ ./test/ ./libs/ -name "*.ts" | xargs tsc -w -t ES5 --sourcemap --outDir bin/gen"
+"C:\Program Files (x86)\Git\bin\sh.exe" --login -i -c "find $(pwd)/src/ $(pwd)/test/ $(pwd)/libs/ -name "*.ts" | xargs node_modules/.bin/tsc -w -t ES5 --sourcemap --outDir bin/gen"
