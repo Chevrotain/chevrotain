fea10252d2ae413f7b5f867f85c840abf9be7cd0
Shahar Soel <shahar.soel@gmail.com>
2015-07-29 00:16:23 +0300

    show definition of selected non-terminal diagram.

---
+++ playground/diagramsHighlights.js
@@ -0,0 +1,68 @@
+// TODO: use css styles instead of hardcoded values
+function attachHighlightEvents() {
+    var diagramHeaders = $(".diagramHeader")
+    _.forEach(diagramHeaders, function (header) {
+        header.addEventListener("mouseover", onDiagramHeaderMouseOver);
+        header.addEventListener("mouseout", onDiagramHeaderMouseOut);
+    })
+
+    var noneTerminals = $(".non-terminal")
+    _.forEach(noneTerminals, function (nonTerminal) {
+        nonTerminal.addEventListener("mouseover", onDiagramNonTerminalMouseOver);
+        nonTerminal.addEventListener("mouseout", onDiagramNonTerminalMouseOut);
+    })
+}
+
+function onDiagramNonTerminalMouseOver(mouseEvent) {
+    var rectAndHeader = getUsageRectAndDefHeader(mouseEvent.target)
+    rectAndHeader.rect.style.fill = "yellow"
+    rectAndHeader.header.style["background-color"] = "#31BA5F"
+}
+
+function onDiagramNonTerminalMouseOut(mouseEvent) {
+    var rectAndHeader = getUsageRectAndDefHeader(mouseEvent.target)
+    rectAndHeader.rect.style.fill = "#A8C1FF"
+    rectAndHeader.header.style["background-color"] = "transparent"
+}
+
+
+function onDiagramHeaderMouseOver(mouseEvent) {
+    var definitionName = mouseEvent.target.innerHTML
+    mouseEvent.target.style["background-color"] = "#31BA5F"
+    _.forEach(getUsageSvgRect(definitionName), function (rect) {
+        rect.style.fill = "yellow"
+    })
+}
+
+function onDiagramHeaderMouseOut(mouseEvent) {
+    var definitionName = mouseEvent.target.innerHTML
+    mouseEvent.target.style["background-color"] = "transparent"
+    _.forEach(getUsageSvgRect(definitionName), function (rect) {
+        rect.style.fill = "#A8C1FF"
+    })
+}
+
+function getUsageSvgRect(definitionName) {
+    var rects = $(".non-terminal").find("rect")
+    return _.filter(rects, function (rect) {
+        var textNode = rect.parentNode.getElementsByTagName('text')[0];
+        return textNode.innerHTML === definitionName
+    })
+}
+
+function getUsageRectAndDefHeader(target) {
+    var rect, text
+    if (target instanceof SVGRectElement) {
+        rect = target
+        text = _.first($(target).siblings("text")).innerHTML
+    }
+    else {
+        rect = _.first($(target).siblings("rect"))
+        text = target.innerHTML
+    }
+    var header = _.find($(".diagramHeader"), function (currHeader) {
+        return currHeader.innerHTML === text;
+    })
+
+    return {rect: rect, header: header}
+}
