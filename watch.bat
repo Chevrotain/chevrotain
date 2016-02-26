a46d078f76d2621d97ecdc535c3ed805b784c165
Shahar Soel <shahar.soel@gmail.com>
2016-02-26 17:30:39 +0200

    watch.bat no longer contains path to "sh" binary. sh binary must be in the path.

--- watch.bat
+++ watch.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Git\bin\sh.exe" --login -i -c "find $(pwd)/src/ $(pwd)/test/ $(pwd)/libs/ -name "*.ts" | xargs node_modules/.bin/tsc -w -t ES5 --diagnostics --sourcemap --module commonjs --outDir bin"
+sh --login -i -c "find $(pwd)/src/ $(pwd)/test/ $(pwd)/libs/ -name "*.ts" | xargs node_modules/.bin/tsc -w -t ES5 --diagnostics --sourcemap --module commonjs --outDir bin"
