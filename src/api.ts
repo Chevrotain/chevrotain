8e0ff3af2892e7cc4c91df0f39a20df690c791db
Shahar Soel <shahar.soel@gmail.com>
2016-05-29 21:55:56 +0300

    release 0.10.0

--- src/api.ts
+++ src/api.ts
@@ -13,7 +13,7 @@ import {NextAfterTokenWalker} from "./parse/grammar/interpreter"
 let API:any = {}
 
 // semantic version
-API.VERSION = "0.9.0"
+API.VERSION = "0.10.0"
 
 // runtime API
 API.Parser = Parser
