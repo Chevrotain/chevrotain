81d09920e646c704328725ee9c6b7fcca881fd82
Shahar Soel <shahar.soel@gmail.com>
2016-09-20 12:05:49 +0300

    Token Types Performance Page: * Fix typo in html * Fix layout by making the iframes have no size.

--- performance/token_types/parsers/loader.js
+++ performance/token_types/parsers/loader.js
@@ -2,6 +2,8 @@ var base = {};
 var baseIframe = document.createElement("iframe");
 baseIframe.setAttribute("src", "parsers/base_parser.html");
 baseIframe.style.visibility = "hidden"
+baseIframe.style.height = 0
+baseIframe.style.width = 0
 document.body.appendChild(baseIframe);
 baseIframe.addEventListener("load", function () {
     base.parser = baseIframe.contentWindow.parser
@@ -12,6 +14,8 @@ var lazy = {};
 var lazyIframe = document.createElement("iframe");
 lazyIframe.setAttribute("src", "parsers/lazy_parser.html");
 lazyIframe.style.visibility = "hidden"
+lazyIframe.style.height = 0
+lazyIframe.style.width = 0
 document.body.appendChild(lazyIframe);
 lazyIframe.addEventListener("load", function () {
     lazy.parser = lazyIframe.contentWindow.parser
@@ -22,6 +26,8 @@ var simpleLazy = {};
 var simpleLazyIframe = document.createElement("iframe");
 simpleLazyIframe.setAttribute("src", "parsers/simple_lazy_parser.html");
 simpleLazyIframe.style.visibility = "hidden"
+simpleLazyIframe.style.height = 0
+simpleLazyIframe.style.width = 0
 document.body.appendChild(simpleLazyIframe);
 simpleLazyIframe.addEventListener("load", function () {
     simpleLazy.parser = simpleLazyIframe.contentWindow.parser
