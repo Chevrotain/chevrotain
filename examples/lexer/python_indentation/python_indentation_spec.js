0ad1d52a7c868c4a944a579ef8fc9d96c3706306
Christian Voigt <1pxsolidblack@googlemail.com>
2017-02-13 15:56:31 +0100

    Adds multiple Outdent tokens support in python indentation example
    Resolves: #366

--- examples/lexer/python_indentation/python_indentation_spec.js
+++ examples/lexer/python_indentation/python_indentation_spec.js
@@ -8,14 +8,12 @@ describe('The Chevrotain Lexer ability to lex python like indentation.', () => {
 
     it('Can Lex a simple python style if-else ', () => {
         let input =
-            "if (1)\n" +
-            "  print 5\n" +
-            "else\n" +
-            "  if (2)\n" +
-            "    print 666\n" +
-            "  else\n" +
-            "    print 999\n" +
-            ""
+        "if (1)\n" +
+        "  if (2)\n" +
+        "    if(3)\n" +
+        "      print 666\n" +
+        "  else\n" +
+        "    print 999\n"
 
         let lexResult = tokenize(input)
         let actualTokens = lexResult.tokens
@@ -24,10 +22,10 @@ describe('The Chevrotain Lexer ability to lex python like indentation.', () => {
         expect(actualTokens[2]).to.be.an.instanceOf(indentationExample.IntegerLiteral)
         expect(actualTokens[3]).to.be.an.instanceOf(indentationExample.RParen)
         expect(actualTokens[4]).to.be.an.instanceOf(indentationExample.Indent)
-        expect(actualTokens[5]).to.be.an.instanceOf(indentationExample.Print)
-        expect(actualTokens[6]).to.be.an.instanceOf(indentationExample.IntegerLiteral)
-        expect(actualTokens[7]).to.be.an.instanceOf(indentationExample.Outdent)
-        expect(actualTokens[8]).to.be.an.instanceOf(indentationExample.Else)
+        expect(actualTokens[5]).to.be.an.instanceOf(indentationExample.If)
+        expect(actualTokens[6]).to.be.an.instanceOf(indentationExample.LParen)
+        expect(actualTokens[7]).to.be.an.instanceOf(indentationExample.IntegerLiteral)
+        expect(actualTokens[8]).to.be.an.instanceOf(indentationExample.RParen)
         expect(actualTokens[9]).to.be.an.instanceOf(indentationExample.Indent)
         expect(actualTokens[10]).to.be.an.instanceOf(indentationExample.If)
         expect(actualTokens[11]).to.be.an.instanceOf(indentationExample.LParen)
@@ -37,10 +35,12 @@ describe('The Chevrotain Lexer ability to lex python like indentation.', () => {
         expect(actualTokens[15]).to.be.an.instanceOf(indentationExample.Print)
         expect(actualTokens[16]).to.be.an.instanceOf(indentationExample.IntegerLiteral)
         expect(actualTokens[17]).to.be.an.instanceOf(indentationExample.Outdent)
-        expect(actualTokens[18]).to.be.an.instanceOf(indentationExample.Else)
-        expect(actualTokens[19]).to.be.an.instanceOf(indentationExample.Indent)
-        expect(actualTokens[20]).to.be.an.instanceOf(indentationExample.Print)
-        expect(actualTokens[21]).to.be.an.instanceOf(indentationExample.IntegerLiteral)
-        expect(actualTokens[17]).to.be.an.instanceOf(indentationExample.Outdent)
+        expect(actualTokens[18]).to.be.an.instanceOf(indentationExample.Outdent)
+        expect(actualTokens[19]).to.be.an.instanceOf(indentationExample.Else)
+        expect(actualTokens[20]).to.be.an.instanceOf(indentationExample.Indent)
+        expect(actualTokens[21]).to.be.an.instanceOf(indentationExample.Print)
+        expect(actualTokens[22]).to.be.an.instanceOf(indentationExample.IntegerLiteral)
+        expect(actualTokens[23]).to.be.an.instanceOf(indentationExample.Outdent)
+        expect(actualTokens[24]).to.be.an.instanceOf(indentationExample.Outdent)
     })
 })
