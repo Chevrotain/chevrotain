64cc6eace405754f4e7b91ab7249c162875f1d2f
Soel <shachar.soel@sap.com>
2017-07-08 18:33:09 +0300

    Run only parser in the benchmark.
    without lexer...
    
    part of #517

--- benchmark_web/parsers/api.js
+++ benchmark_web/parsers/api.js
@@ -1,10 +1,13 @@
 // ----------------- wrapping it all together -----------------
 var parserInstance
+var lexResult
 
-function parseBench(text, lexOnly, lexer, parser, rootRule) {
-    var lexResult = lexer.tokenize(text)
-    if (lexResult.errors.length > 0) {
-        throw Error("Lexing errors detected")
+function parseBench(text, lexer, parser, rootRule, options) {
+    if (lexResult === undefined || options.lexerOnly) {
+        lexResult = lexer.tokenize(text)
+        if (lexResult.errors.length > 0) {
+            throw Error("Lexing errors detected")
+        }
     }
 
     // It is recommended to only initialize a Chevrotain Parser once
@@ -13,11 +16,17 @@ function parseBench(text, lexOnly, lexer, parser, rootRule) {
         parserInstance = new parser([])
     }
 
-    if (lexOnly) {
+    if (options.lexerOnly) {
         return lexResult.tokens
     } else {
         // setting a new input will RESET the parser instance's state.
         parserInstance.input = lexResult.tokens
+        var lexErrors = lexResult.errors
+
+        // only performing the lexing ONCE if we are only interested in the parsing speed
+        if (!options.parserOnly) {
+            lexResult = undefined
+        }
 
         // any top level rule may be used as an entry point
         var value = parserInstance[rootRule]()
@@ -27,7 +36,7 @@ function parseBench(text, lexOnly, lexer, parser, rootRule) {
         }
         return {
             value: value, // this is a pure grammar, the value will always be <undefined>
-            lexErrors: lexResult.errors,
+            lexErrors: lexErrors,
             parseErrors: parserInstance.errors
         }
     }
