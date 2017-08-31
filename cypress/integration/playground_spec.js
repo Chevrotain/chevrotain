4528041437ed6113c7350f9bbe3521f3c4b573b6
Soel <shachar.soel@sap.com>
2017-08-31 13:59:57 +0300

    Improved performance page tests.
    * Ohm can't be tested on travis as Ohm is
      too slow and we hit even a "crazy" 200seconds timeut...
    
    * Use direct dom querying as it is so easy with cypress :)
    
    * un-toggle/Toggle all the checkboxes in the UI.

--- cypress/integration/playground_spec.js
+++ cypress/integration/playground_spec.js
@@ -1,21 +1,32 @@
-describe('My First Test', function () {
+describe('Performance Page Test', function () {
 
     it("clicking 'root' shows the right headings", function () {
         cy.visit('http://sap.github.io/chevrotain/performance/')
 
+        // ensure the result table is empty before we started
         cy.get(".value:visible").then((vals) => {
             vals.each((i, val) => {
                 expect(val.innerHTML).to.eq("&nbsp;")
             })
         })
 
-        cy.get(".Ohm").find(":checkbox").check()
+        cy.get(":checkbox:visible").uncheck().check()
+        // Ohm is too slow to be benched in CI containers.
+        cy.get(".Ohm").find(":checkbox").uncheck()
+
+        // start The benchmark
         cy.get('#runAllButton').click()
 
+        // the runAllButton changing it's text to Re-run would indicate the end of the benchmark.
         cy.get('#runAllButton').contains('Re-run', {timeout: 200000}).then((x) => {
             cy.get(".value:visible").then((vals) => {
                 vals.each((i, val) => {
-                    expect(/\d+(\.\d+)?/.test(val.innerHTML)).to.be.true
+                    // working directly on the DOM because my JQuery skills are weak :)
+                    // I don't think this direct access would even be possible in selenium.
+                    if (val.parentElement.parentElement.classList[1] !== "Ohm") {
+                        // ensure the result table has valid numerical results
+                        expect(/\d+(\.\d+)?/.test(val.innerHTML)).to.be.true
+                    }
                 })
             })
         })
