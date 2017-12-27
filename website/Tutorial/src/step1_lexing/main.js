fc3eddc3fdac4643a16562baeea901338b4b16c9
Soel <shachar.soel@sap.com>
2017-12-27 22:32:54 +0200

    Updating Website

--- website/Tutorial/src/step1_lexing/main.js
+++
@@ -1,5 +0,0 @@
-const lex = require("./step1_lexing").lex
-
-let inputText = "SELECT column1 FROM table2"
-let lexingResult = lex(inputText)
-console.log(JSON.stringify(lexingResult, null, "\t"))
--- website/Tutorial/src/step2_parsing/main.js
+++
@@ -1,7 +0,0 @@
-const parse = require("./step2_parsing").parse
-
-let inputText = "SELECT column1 FROM table2"
-// step into the parse function to debug the full flow
-parse(inputText)
-
-// no output here so nothing to show...
