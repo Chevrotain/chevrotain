4c906e12fb9e832ee6310013cf8b49b25a04033a
Soel <shachar.soel@sap.com>
2017-12-27 02:03:36 +0200

    release 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## X.Y.Z (INSERT_DATE_HERE)
+## 1.0.1 (12-27-2017)
 
 #### Documentation
 
--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // needs a separate module as this is required inside chevrotain productive code
 // and also in the entry point for webpack(api.ts).
 // A separate file avoids cyclic dependencies and webpack errors.
-export const VERSION = "1.0.0"
+export const VERSION = "1.0.1"
