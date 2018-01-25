6d6cd49ba77d87faa30665a4353685a0d4d0693d
Shahar Soel <shahar.soel@gmail.com>
2018-01-25 11:18:19 +0200

    Missing docs link in deprecation check. (#641)
    Fixes #640

--- src/scan/tokens_public.ts
+++ src/scan/tokens_public.ts
@@ -123,7 +123,7 @@ function createTokenInternal(config: ITokenConfig): TokenType {
 
     if (has(config, PARENT)) {
         throw "The parent property is no longer supported.\n" +
-            "See: [TODO-add link] for details."
+            "See: https://github.com/SAP/chevrotain/issues/564#issuecomment-349062346 for details."
     }
 
     if (has(config, CATEGORIES)) {
