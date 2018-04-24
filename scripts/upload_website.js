01b81d0c71046f04d902272233bf1f410fbf4e4c
Soel <shachar.soel@sap.com>
2018-04-24 18:40:41 +0300

    upload website script updated.

--- scripts/upload_website.js
+++ scripts/upload_website.js
@@ -1,27 +1,28 @@
 // TODO: This is a quick and dirty copy paste.
 // Need to refactor this to avoid duplication with upload_docs.js script.
-var git = require('gitty')
-var _ = require('lodash')
-var wrench = require('wrench')
-var path = require('path')
+const git = require('gitty');
+const _ = require('lodash');
+const fs = require('fs-extra');
+const path = require('path');
 
-var myRepo = git(path.join(__dirname, '../'))
-var status = myRepo.statusSync()
+const myRepo = git(path.join(__dirname, '../'));
+const status = myRepo.statusSync();
 if (!_.isEmpty(status.staged) || !_.isEmpty(status.unstaged) || !_.isEmpty(status.untracked)) {
     console.error('Error: git working directory must be clean in order to upload the website')
     process.exit(-1)
 }
 
-var branchesInfo = myRepo.getBranchesSync()
+const branchesInfo = myRepo.getBranchesSync();
 
 if (branchesInfo.current !== 'gh-pages') {
     console.error('Error: can only perform website upload job from gh-pages branch')
     process.exit(-1)
 }
 
-var targetWebsiteDir = path.join(__dirname, '../website/')
-var orgWebsiteLocation = path.join(__dirname, '../../chevrotain/dev/website')
-wrench.copyDirSyncRecursive(orgWebsiteLocation, targetWebsiteDir, {forceDelete: true})
+const targetWebsiteDir = path.join(__dirname, '../docs/');
+fs
+const orgWebsiteLocation = path.join(__dirname, '../../chevrotain/docs/.vuepress/dist');
+fs.moveSync(orgWebsiteLocation, targetWebsiteDir, {overwrite: true})
 
 myRepo.addSync([targetWebsiteDir])
 myRepo.commitSync("Updating Website")
