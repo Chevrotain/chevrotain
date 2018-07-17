33a2c3568ea5cdf757d2dc8466f008db9526a54b
Shahar Soel <4233843+bd82@users.noreply.github.com>
2018-07-17 03:22:06 +0300

    Graphql example (#780)

---
+++ examples/grammars/graphql/graphql_spec.js
@@ -0,0 +1,59 @@
+const { expect } = require("chai")
+const { parse } = require("./graphql")
+
+describe("The GraphQL Grammar", () => {
+    it("can parse a simple GraphQL without errors", () => {
+        const input = `
+{
+  hero {
+    name
+    # Queries can have comments!
+    friends {
+      name
+    }
+  }
+}
+`
+        const parseResult = parse(input)
+        expect(parseResult.lexErrors).to.be.empty
+        expect(parseResult.parseErrors).to.be.empty
+    })
+
+    it("can parse a simple GraphQL without errors #2", () => {
+        const input = `
+{
+    human(id: "1000") {
+    name
+    height(unit: FOOT)
+}
+}
+`
+        const parseResult = parse(input)
+        expect(parseResult.lexErrors).to.be.empty
+        expect(parseResult.parseErrors).to.be.empty
+    })
+
+    it("can parse a simple GraphQL without errors #3", () => {
+        const input = `
+type Human implements Character {
+  id: ID!
+  name: String!
+  friends: [Character]
+  appearsIn: [Episode]!
+  starships: [Starship]
+  totalCredits: Int
+}
+
+type Droid implements Character {
+  id: ID!
+  name: String!
+  friends: [Character]
+  appearsIn: [Episode]!
+  primaryFunction: String
+}
+`
+        const parseResult = parse(input)
+        expect(parseResult.lexErrors).to.be.empty
+        expect(parseResult.parseErrors).to.be.empty
+    })
+})
