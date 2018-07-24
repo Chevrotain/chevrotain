4444fb0bf4b046f7aab0135ba57657d17edeac8b
Shahar Soel <shahars@protonmail.com>
2018-07-24 18:09:38 +0300

    Fix formatting.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -163,7 +163,10 @@ module.exports = {
                         ["syntax_diagrams", "Syntax Diagrams"],
                         ["regexp", "RegExp Based Lexers"],
                         ["position_tracking", "Position Tracking"],
-                        ["token_alternative_matches", "Token Alternative Matches"],
+                        [
+                            "token_alternative_matches",
+                            "Token Alternative Matches"
+                        ],
                         ["token_skipping", "Token Skipping"],
                         ["token_grouping", "Token Grouping"]
                     ]
