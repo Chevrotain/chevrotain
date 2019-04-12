18944c9172773a36cb4f218bc92f31695a6ec318
Shahar Soel <shahars@protonmail.com>
2019-04-12 20:16:15 +0300

    - Avoid  hoisting for karma configurations. - Add the Parsing Rules on the prototype of the Parser of avoid performance regressions   due to none fast property access in more recent chrome versions.

--- packages/chevrotain/src/parse/parser/traits/recognizer_api.ts
+++ packages/chevrotain/src/parse/parser/traits/recognizer_api.ts
@@ -622,7 +622,7 @@ export class RecognizerApi {
         }
 
         let ruleImplementation = this.defineRule(name, implementation, config)
-        this[name] = ruleImplementation
+        this.constructor.prototype[name] = ruleImplementation
         return ruleImplementation
     }
 
@@ -653,7 +653,7 @@ export class RecognizerApi {
         }
 
         let ruleImplementation = this.defineRule(name, impl, config)
-        this[name] = ruleImplementation
+        this.constructor.prototype[name] = ruleImplementation
         return ruleImplementation
     }
 
