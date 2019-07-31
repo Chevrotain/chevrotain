91fa38a4410089046731a3b041a5b0d5cab71edc
bd82 <shahars@protonmail.com>
2019-08-01 00:05:26 +0300

    fix preversion generation

--- packages/chevrotain/src/version.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-exports.__esModule = true
-// needs a separate module as this is required inside chevrotain productive code
-// and also in the entry point for webpack(api.ts).
-// A separate file avoids cyclic dependencies and webpack errors.
-exports.VERSION = "4.8.1"
