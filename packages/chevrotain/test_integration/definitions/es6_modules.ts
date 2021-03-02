dd56d82805800e1aefcecf33dfd7e5f8f497cdcb
Shahar Soel <4233843+bd82@users.noreply.github.com>
2021-03-02 21:45:02 +0200

    refactor: extract public APIs definitions to a separate package (#1396)

--- packages/chevrotain/test_integration/definitions/es6_modules.ts
+++
@@ -1,6 +0,0 @@
-import { EmbeddedActionsParser, CstParser, Lexer } from "../../lib/chevrotain"
-
-/**
- * This File will be compiled using TSC to verify that the custom build chevrotain.d.ts
- * is compatible with Typescript's ES6 module syntax (import/export)
- */
--- packages/chevrotain/test_integration/definitions/namespaces.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../lib/chevrotain.d.ts" />
-import Lexer = chevrotain.Lexer
-import EmbeddedActionsParser = chevrotain.EmbeddedActionsParser
-import CstParser = chevrotain.CstParser
-
-/**
- * This File will be compiled using TSC to verify that the custom build chevrotain.d.ts
- * is compatible with Typescript's old style module/namespaces (global)
- */
