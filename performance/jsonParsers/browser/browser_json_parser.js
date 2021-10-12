8c7fa976f48c15f3806474b63ca585645aea27ca
Shahar Soel <shahars@protonmail.com>
2021-10-13 00:33:36 +0300

    docs: make the benchmark more apples to apples
    By removing Parse Tree / Listener variants and focusing
    on the pure parsing flow.
    
    Also removed some none Parser libraries variants
    which were already hidden.

--- performance/jsonParsers/browser/browser_json_parser.js
+++
@@ -1,8 +0,0 @@
-
-var x;
-(function (exports) {
-
-  exports.parse_json_browser = function parse_json_browser(input) {
-    x = JSON.parse(input);
-  }
-})(this);
--- performance/jsonParsers/jsonify/jsonify_json_parser.js
+++
@@ -1,7 +0,0 @@
-
-var x;
-(function (exports) {
-  exports.parse_json_jsonify = function parse_json_jsonify(input) {
-    x = jsonifyParse(input);
-  }
-})(this);
