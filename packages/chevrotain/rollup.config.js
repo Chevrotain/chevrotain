a3c447226328cb72074ac98a77e4a54af14d1ef1
Adrian Leonhard <adrianleonhard@gmail.com>
2021-11-03 17:43:07 +0100

    fixes

--- packages/chevrotain/rollup.config.js
+++ packages/chevrotain/rollup.config.js
@@ -4,48 +4,98 @@ const typescript = require("typescript")
 const { terser } = require("rollup-plugin-terser")
 const commonjsPlugin = require("@rollup/plugin-commonjs")
 
-module.exports = {
-  input: "src/api.ts",
-  output: [
-    ["es", false],
-    ["es", true],
-    ["umd", false],
-    ["umd", true]
-  ].map(([format, compress]) => ({
-    format: format,
-    entryFileNames:
-      "chevrotain-bundle" +
-      (compress ? ".min" : "") +
-      (format === "es" ? ".mjs" : ".js"),
-    sourcemap: true,
-    dir: "lib",
-    name: "chevrotain",
-    exports: "named",
-    plugins: compress
-      ? [
-          terser({
-            compress: {
-              passes: 3,
-              unsafe: true,
-              ecma: 7
-            },
-            toplevel: true,
-            mangle: {
-              properties: { regex: /^_/ }
-            }
-          })
-        ]
-      : []
-  })),
-  external: {},
-  plugins: [
-    typescriptPlugin({ typescript }),
-    nodeResolvePlugin(),
-    commonjsPlugin()
-  ],
-  onwarn: function (warning, warn) {
-    if ("THIS_IS_UNDEFINED" === warning.code) return
-    // if ('CIRCULAR_DEPENDENCY' === warning.code) return
-    warn(warning)
+const pkg = require("./package.json")
+
+module.exports = [
+  // no deps
+  {
+    input: "src/api.ts",
+    output: [
+      ["commonjs", false],
+      ["es", false]
+    ].map(([format, compress]) => ({
+      format: format,
+      entryFileNames:
+        "chevrotain" +
+        (compress ? ".min" : "") +
+        (format === "es" ? ".mjs" : ".js"),
+      sourcemap: true,
+      dir: "lib",
+      name: "chevrotain",
+      exports: "named",
+      plugins: compress
+        ? [
+            terser({
+              compress: {
+                passes: 3,
+                unsafe: true,
+                ecma: 7
+              },
+              toplevel: true,
+              mangle: {
+                properties: { regex: /^_/ }
+              }
+            })
+          ]
+        : []
+    })),
+    external: Object.keys(pkg.dependencies),
+    plugins: [
+      typescriptPlugin({ typescript }),
+      nodeResolvePlugin(),
+      commonjsPlugin()
+    ],
+    onwarn: function (warning, warn) {
+      if ("THIS_IS_UNDEFINED" === warning.code) return
+      // if ('CIRCULAR_DEPENDENCY' === warning.code) return
+      warn(warning)
+    }
+  },
+
+  // bundle
+  {
+    input: "src/api.ts",
+    output: [
+      ["es", false, true],
+      ["es", true, true],
+      ["umd", false, true],
+      ["umd", true, true]
+    ].map(([format, compress]) => ({
+      format: format,
+      entryFileNames:
+        "chevrotain-bundle" +
+        (compress ? ".min" : "") +
+        (format === "es" ? ".mjs" : ".js"),
+      sourcemap: true,
+      dir: "lib",
+      name: "chevrotain",
+      exports: "named",
+      plugins: compress
+        ? [
+            terser({
+              compress: {
+                passes: 3,
+                unsafe: true,
+                ecma: 7
+              },
+              toplevel: true,
+              mangle: {
+                properties: { regex: /^_/ }
+              }
+            })
+          ]
+        : []
+    })),
+    external: [],
+    plugins: [
+      typescriptPlugin({ typescript }),
+      nodeResolvePlugin(),
+      commonjsPlugin()
+    ],
+    onwarn: function (warning, warn) {
+      if ("THIS_IS_UNDEFINED" === warning.code) return
+      // if ('CIRCULAR_DEPENDENCY' === warning.code) return
+      warn(warning)
+    }
   }
-}
+]
