ed4c6ff98aede8361a0d8b6bd6d3685d7a9be9d1
bd82 <shahars@protonmail.com>
2022-01-10 02:17:22 +0200

    test: additional test cases

--- packages/gast/test/helpers_spec.ts
+++ packages/gast/test/helpers_spec.ts
@@ -1,10 +1,9 @@
 import { expect } from "chai"
 import type { ITokenConfig, TokenType } from "@chevrotain/types"
 import {
-  GAstVisitor,
+  Alternation,
   Terminal,
   Rule,
-  Alternation,
   Alternative,
   Repetition,
   RepetitionWithSeparator,
@@ -25,10 +24,11 @@ function createDummyToken(opts: ITokenConfig): TokenType {
 
 describe("the gast helper utilities", () => {
   let A: TokenType
+  let B: TokenType
 
-  // TODO: do we need this at all?
   before(() => {
     A = createDummyToken({ name: "A" })
+    B = createDummyToken({ name: "B" })
   })
 
   context("isSequenceProd()", () => {
@@ -107,6 +107,19 @@ describe("the gast helper utilities", () => {
 
   context("isOptionalProd()", () => {
     context("positive for", () => {
+      it("an Alternation where some alternative is empty", () => {
+        const prod = new Alternation({
+          definition: [
+            new Alternative({
+              definition: [new Terminal({ terminalType: A })]
+            }),
+            new Alternative({ definition: [] })
+          ]
+        })
+        const result = isOptionalProd(prod)
+        expect(result).to.be.true
+      })
+
       it("Option", () => {
         const prod = new Option({ definition: [] })
         const result = isOptionalProd(prod)
@@ -130,8 +143,23 @@ describe("the gast helper utilities", () => {
     })
 
     context("negative for", () => {
-      it("Alternative", () => {
-        const prod = new Alternative({ definition: [] })
+      it("an Alternation where every alternative is non-empty", () => {
+        const prod = new Alternation({
+          definition: [
+            new Alternative({
+              definition: [new Terminal({ terminalType: A })]
+            }),
+            new Alternative({ definition: [new Terminal({ terminalType: B })] })
+          ]
+        })
+        const result = isOptionalProd(prod)
+        expect(result).to.be.false
+      })
+
+      it("non empty Alternative", () => {
+        const prod = new Alternative({
+          definition: [new Terminal({ terminalType: A })]
+        })
         const result = isOptionalProd(prod)
         expect(result).to.be.false
       })
@@ -142,15 +170,17 @@ describe("the gast helper utilities", () => {
         expect(result).to.be.false
       })
 
-      it("RepetitionMandatory", () => {
-        const prod = new RepetitionMandatory({ definition: [] })
+      it("non-empty RepetitionMandatory", () => {
+        const prod = new RepetitionMandatory({
+          definition: [new Terminal({ terminalType: A })]
+        })
         const result = isOptionalProd(prod)
         expect(result).to.be.false
       })
 
-      it("RepetitionMandatoryWithSeparator", () => {
+      it("non-empty RepetitionMandatoryWithSeparator", () => {
         const prod = new RepetitionMandatoryWithSeparator({
-          definition: [],
+          definition: [new Terminal({ terminalType: A })],
           separator: A
         })
         const result = isOptionalProd(prod)
@@ -165,9 +195,25 @@ describe("the gast helper utilities", () => {
         expect(result).to.be.false
       })
 
-      it("Rule", () => {
-        const prod = new Rule({ name: "foo", definition: [] })
+      it("none empty Rule", () => {
+        const prod = new Rule({
+          name: "foo",
+          definition: [new Terminal({ terminalType: A })]
+        })
         const result = isOptionalProd(prod)
+        expect(result).to.be.false
+      })
+    })
+
+    context("edge case", () => {
+      it("will avoid infinite loop on recursive Non-Terminals", () => {
+        const recursiveRule = new Rule({ name: "recursive", definition: [] })
+        const recursiveNonTerminal = new NonTerminal({
+          nonTerminalName: "recursive",
+          referencedRule: recursiveRule
+        })
+        recursiveRule.definition = [recursiveNonTerminal]
+        const result = isOptionalProd(recursiveNonTerminal)
         expect(result).to.be.false
       })
     })
