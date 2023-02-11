7ccdb02af2eb52379f87e880eae2f98eb8d5b30a
nupthale <360783395@qq.com>
2023-02-12 07:01:05 +0800

    feat: add IRecognitionException to recoveryValueFunc arguments to provide information (#1917)
    Co-authored-by: hale <hale@bytedance.com>

--- packages/chevrotain/src/parse/parser/traits/recognizer_engine.ts
+++ packages/chevrotain/src/parse/parser/traits/recognizer_engine.ts
@@ -281,7 +281,7 @@ export class RecognizerEngine {
             partialCstResult.recoveredNode = true
             return partialCstResult
           } else {
-            return recoveryValueFunc()
+            return recoveryValueFunc(e)
           }
         } else {
           if (this.outputCst) {
@@ -298,7 +298,7 @@ export class RecognizerEngine {
         this.moveToTerminatedState()
         // the parser should never throw one of its own errors outside its flow.
         // even if error recovery is disabled
-        return recoveryValueFunc()
+        return recoveryValueFunc(e)
       } else {
         // to be recovered Further up the call stack
         throw recogError
