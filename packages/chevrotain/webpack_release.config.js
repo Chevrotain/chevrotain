6911cbfb4c53aa5bc3fe79fdf27940a4753fd5be
Shahar Soel <shachar.soel@sap.com>
2023-07-13 16:52:56 +0300

    chore: remove unused webpack configs

--- packages/chevrotain/webpack_release.config.js
+++
@@ -1,6 +0,0 @@
-import { createWebpackConfig } from "./webpack_base.config.js";
-
-export default createWebpackConfig({
-  minimize: false,
-  filename: "chevrotain.internal.temp.js",
-});
--- packages/chevrotain/webpack_release_min.config.js
+++
@@ -1,6 +0,0 @@
-import { createWebpackConfig } from "./webpack_base.config.js";
-
-export default createWebpackConfig({
-  minimize: true,
-  filename: "chevrotain.internal.temp.min.js",
-});
