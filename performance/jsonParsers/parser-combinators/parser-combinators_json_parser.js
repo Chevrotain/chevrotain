19b755384c689b49ee000224a10ff323fbef1b6b
Michał Isalski <isalski.michal@gmail.com>
2026-01-18 20:58:51 +0100

    devux(benchmark): used lazy parser in parser-combinators benchmark

--- performance/jsonParsers/parser-combinators/parser-combinators_json_parser.js
+++ performance/jsonParsers/parser-combinators/parser-combinators_json_parser.js
@@ -1,4 +1,4 @@
-import { str, between, regex, any, seq, zeroOrMany, ParseText } from 'https://unpkg.com/parser-combinators/dist-esm/index.js';
+import { any, between, lazy, regex, seq, str, zeroOrMany, ParseText } from 'https://unpkg.com/parser-combinators/dist-esm/index.js';
 
 const WhiteSpace = regex(/\s*/m, 'whitespace');
 
@@ -15,37 +15,33 @@ const Colon = str(":");
 const StringLiteral = regex(/"(?:[^\\"]|\\(?:[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/, 'string literal');
 const NumberLiteral = regex(/-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/, 'number literal');
 
+const JArray = lazy(() => seq(
+    LSquare,
+    WhiteSpace,
+    zeroOrMany(Value, Comma),
+    WhiteSpace,
+    RSquare
+));
+
+const JObject = lazy(() => seq(
+    LCurly,
+    WhiteSpace,
+    zeroOrMany(ObjectEntry, Comma),
+    WhiteSpace,
+    RCurly
+));
+
 const Value = any(
     StringLiteral,
     NumberLiteral,
-    JObject(),
-    JArray(),
+    JObject,
+    JArray,
     True,
     False,
     Null,
 );
 
 const ObjectEntry = seq(StringLiteral, WhiteSpace, Colon, WhiteSpace, Value);
-
-function JArray() {
-    return (ctx) => seq(
-        LSquare,
-        WhiteSpace,
-        zeroOrMany(Value, Comma),
-        WhiteSpace,
-        RSquare
-    )(ctx);
-};
-
-function JObject() {
-    return (ctx) => seq(
-        LCurly,
-        WhiteSpace,
-        zeroOrMany(ObjectEntry, Comma),
-        WhiteSpace,
-        RCurly
-    )(ctx);
-}
 
 const json = between(WhiteSpace, Value, WhiteSpace);
 
