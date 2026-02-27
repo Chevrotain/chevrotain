4c463c7ed5a407b2607d1ee5dee5906a8156f66e
Shahar Soel <shachar.soel@sap.com>
2026-02-27 21:23:16 +0200

    Update Website

--- docs/assets/404.html-C2TTMnq4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,b as n,o as a}from"./app-DH9kC7jn.js";const l={};function r(s,t){return a(),o("div",null,[...t[0]||(t[0]=[n("p",null,"404 Not Found",-1)])])}const _=e(l,[["render",r]]),i=JSON.parse('{"path":"/404.html","title":"","lang":"en-US","frontmatter":{"layout":"NotFound"},"git":{},"filePathRelative":null}');export{_ as comp,i as data};
